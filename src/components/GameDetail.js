8389b13fd93eded0dc93b53140bec11080589c9d
iam-ljk <jay4luhar@gmail.com>
2021-05-01 15:10:16 +0530

    Data fetched and start styling

---
+++ src/components/GameDetail.js
@@ -0,0 +1,81 @@
+import React from 'react';
+//Styling and Animation 
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+//REDUX 
+import { useSelector } from 'react-redux';
+
+const GameDetail = () => {
+    //Data
+    const { screen, game } = useSelector(state => state.detail);
+    return (
+        <CardShadow>
+            <Detail>
+                <div className="stats">
+                    <div className="rating">
+                        <h3>{game.name}</h3>
+                        <p>Rating : {game.rating}</p>
+                    </div>
+                    <div className="info">
+                        <h3>Platforms</h3>
+                        <div className="platforms">
+                            {game.platforms.map(data => (
+                                <h3 key={data.platform.id}>{data.platform.name}</h3>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                <div className="media">
+                    <img src={game.background_image} alt='media' /> 
+                </div>
+                <div className="description">
+                    <p>{game.description_raw}</p>
+                </div>
+                <div className="gallary">
+                    {screen.results.map(screen => (
+                        <img src={screen.image} key={screen.id} alt="game" />
+                    ))}
+                </div>
+            </Detail>
+        </CardShadow>
+    );
+
+};
+
+const CardShadow = styled(motion.div)`
+    width: 100%;
+    overflow-y : scroll;
+    min-height: 100vh;
+    background: rgba(0,0,0,0.5);
+    position: fixed;
+    top:0;
+    left:0;
+
+    &::-webkit-scrollbar {
+        width:0.5rem;
+    }
+
+    &::-webkit-scrollbar-thumk {
+        background-color: #ff7676;
+    }
+
+    &::-webkit-scrollbar-track {
+        background: white;
+    }
+`;
+
+const Detail = styled(motion.div)`
+    width:80%;
+    border-radius:1rem;
+    padding:2rem 20rem;
+    background:white;
+    position: absolute;
+    left: 10%;
+    color: black;
+
+    img{
+        width:100%;
+    }
+`;
+
+export default GameDetail;
